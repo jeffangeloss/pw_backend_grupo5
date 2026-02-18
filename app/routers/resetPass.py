67a3bc1e4fb631a49bd9a4ca22aa4fa5c07a85c8
Mariel <casoldam@gmail.com>
2026-02-18 16:16:25 -0500

    reset contra preliminar
    estructura a medias de reset contraseña

---
+++ app/routers/resetPass.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+
+from ..security import create_access_token
+from ..schemas import ResetRequest
+from ..database import get_db
+from ..models import Usuario
+
+router = APIRouter(
+    prefix="/reset-contra",
+    tags = ["Restablecer contra"]
+)
+
+@router.put("/request")
+async def request_reset_contra( request : ResetRequest,  db : Session = Depends(get_db) ):
+    db_usuario = db.query(Usuario).filter(Usuario.email == request.email).first()
+    if db_usuario:
+        # crear token
+        reset_token = create_access_token (
+            {"sub" : db_usuario.email,
+            "type" : "password_reset"},
+            expires_minutes=5
+        )
+
+    # ENVIAR EMAIL
+    # en el email se deberia enviar el token
+
+    return {
+        "msg" : "Se completo el request"
+        #"data" : token BORRAR LUEGO
+    }
+
+@router.post("confirm/")
+async def request_confirm_contra( token: str, db : Session = Depends(get_db) ):
+    pass
+
