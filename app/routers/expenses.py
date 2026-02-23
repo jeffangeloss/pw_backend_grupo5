fa86e3ed175859673377f05ab9bfacfae332b36a
rafaperezul <pdeponte552@gmail.com>
2026-02-23 14:41:04 -0500

    bugfix delete expense

--- app/routers/expenses.py
+++ app/routers/expenses.py
@@ -205,6 +205,5 @@ async def delete_expense(
     db.commit()
     
     return {
-        "msg" : "Egreso eliminado",
-        "data" : _serialize_expense(expense)
+        "msg" : "Egreso eliminado"
     }
