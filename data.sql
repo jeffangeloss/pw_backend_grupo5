f1bc18c649535e68dbd76a1f3e09237bb353d507
jeffangeloss <20235218@aloe.ulima.edu.pe>
2026-02-24 13:16:27 -0500

    data.sql

--- data.sql
+++ data.sql
@@ -1,8 +1,53 @@
-INSERT INTO category (name) VALUES
-('Alimentacion'),
-('Transporte'),
-('Servicios'),
-('Salud'),
-('Educacion'),
-('Otros')
-ON CONFLICT (name) DO NOTHING;
+INSERT INTO "user" (
+  id,
+  full_name,
+  email,
+  password_hash,
+  role,
+  email_verified,
+  is_active
+)
+VALUES
+(
+  '3f695099-1016-4b59-a027-f7e28da0f265',
+  'Usuario Demo',
+  'ejemplo@user.com',
+  '$argon2id$v=19$m=65536,t=3,p=4$lcOx2xcOv/reQXAF3VXilw$Cb+DdfU31Jip2x5LFjYc1lPzrcxjxtD1UNcKAMVUrf0',
+  'user'::user_role,
+  true,
+  true
+),
+(
+  '7f9db8dc-b088-4731-9dd0-2a70e721c822',
+  'Admin Demo',
+  'ejemplo@admin.com',
+  '$argon2id$v=19$m=65536,t=3,p=4$7MsLqdosXJphqkqGlw7HQA$z1BHKiljeIEQa5Jh3mTe+RIuxKtWmHd7jcms7m4CZPQ',
+  'admin'::user_role,
+  true,
+  true
+),
+(
+  '80b44d36-f154-4a08-8cb7-1eec0204cb59',
+  'Owner Demo',
+  'ejemplo@owner.com',
+  '$argon2id$v=19$m=65536,t=3,p=4$7MsLqdosXJphqkqGlw7HQA$z1BHKiljeIEQa5Jh3mTe+RIuxKtWmHd7jcms7m4CZPQ',
+  'owner'::user_role,
+  true,
+  true
+),
+(
+  'a7e2e064-0358-4688-8a24-080e6d024920',
+  'Auditor Demo',
+  'ejemplo@auditor.com',
+  '$argon2id$v=19$m=65536,t=3,p=4$7MsLqdosXJphqkqGlw7HQA$z1BHKiljeIEQa5Jh3mTe+RIuxKtWmHd7jcms7m4CZPQ',
+  'auditor'::user_role,
+  true,
+  true
+)
+ON CONFLICT (email) DO UPDATE
+SET
+  full_name = EXCLUDED.full_name,
+  password_hash = EXCLUDED.password_hash,
+  role = EXCLUDED.role,
+  email_verified = EXCLUDED.email_verified,
+  is_active = EXCLUDED.is_active;
