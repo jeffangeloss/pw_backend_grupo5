5f8ade952bad75b27d6126303b06101188056fdd
jeffangeloss <20235218@aloe.ulima.edu.pe>
2026-02-24 21:41:45 -0500

    variables de entorno para db cloud (render)

--- app/database.py
+++ app/database.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-# USUARIO: finanzas
-# CONTRASEÑA: finanzas$616
-# NOMBRE_DB: finanzas_db
-SQLALCHEMY_DATABASE_URL = "postgresql://finanzas:finanzas$616@localhost:5432/finanzas_db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
